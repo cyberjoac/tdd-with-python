75b0a7cd8ae78a86e448551e8f70a9875efd7a60
Joachim Hagege <cyberjoac@gmail.com>
2015-07-13 15:52:33 +0300

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,5 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
-
-# Create your views here.
 from lists.models import Item, List
 
 
@@ -9,15 +7,9 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'list': list_})
-
-
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -25,10 +17,12 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect('/lists/%d/' % (list_.id))
 
 
-def add_item(request, list_id):
+def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
+    return render(request, 'list.html', {'list': list_})
