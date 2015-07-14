8c758ecdf2d9dd38e8ce51dfed26e2947fd56cf8
Joachim Hagege <cyberjoac@gmail.com>
2015-07-14 20:11:56 +0300

    Implemented logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,7 @@ from accounts import views
 
 urlpatterns = [
     url(r'^login$', views.persona_login, name='persona_login'),
+    url(r'^logout', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+
+
 ]
