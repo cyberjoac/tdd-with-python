a9eb2620180531018d4399de4d6a22e22adefd3e
Joachim Hagege <cyberjoac@gmail.com>
2015-07-15 15:15:07 +0300

    Url, placeholder view, and first-cut templates for my_lists

--- lists/views.py
+++ lists/views.py
@@ -27,3 +27,7 @@ def view_list(request, list_id):
             return redirect(list_)
 
     return render(request, 'list.html', {'list': list_, 'form': form})
+
+def my_lists(request, email):
+    # owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html')
